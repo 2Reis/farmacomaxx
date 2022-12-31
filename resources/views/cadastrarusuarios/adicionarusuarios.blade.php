5770c32b9f1e4a7af93ddda22997a265b7ab53ac
Diego Dos Reis <102711947+2Reis@users.noreply.github.com>
2022-12-30 22:11:33 -0300

    git commit -a

--- resources/views/cadastrarusuarios/adicionarusuarios.blade.php
+++ resources/views/cadastrarusuarios/adicionarusuarios.blade.php
@@ -7,9 +7,37 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Cadastrar Usuarios</h1>    
-    <input class="form-control form-control-lg" type="text" name="name">      
-    <input class="form-control" type="text" name="email">
-    <input class="form-control form-control-sm" type="text" name="senha">     
+    <form class="row gy-2 gx-3 align-items-center">
+        <div class="col-6">
+          <label class="visually-hidden" for="autoSizingInput">Name</label>
+          <input type="text" class="form-control" id="autoSizingInput" placeholder="Nome...">
+        </div>
+        <div class="col-6">
+          <label class="visually-hidden" for="autoSizingInputGroup">User Id</label>
+          <div class="input-group">           
+            <input type="text" class="form-control" id="autoSizingInputGroup" placeholder="Nome de usuário...">
+          </div>
+        </div>
+        <div class="col-auto">
+          <label class="visually-hidden" for="autoSizingSelect">Cargo</label>
+          <select class="form-select" id="autoSizingSelect">
+            <option selected>Choose...</option>
+            <option value="1">Auditor</option>
+            <option value="2">Gerente</option>
+            <option value="3">Colaborador</option>
+          </select>
+        </div>
+        <div class="col-auto">
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="autoSizingCheck">
+            <label class="form-check-label" for="autoSizingCheck">
+              Remember me
+            </label>
+          </div>
+        </div>
+        <div class="col-auto">
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+      </form>
 </body>
 </html>
